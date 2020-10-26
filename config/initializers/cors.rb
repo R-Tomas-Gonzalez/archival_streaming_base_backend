722fb750b6884b022143f975fd644d0e46384cd7
R-Tomas-Gonzalez <tomas@rtomasgonzalez.com>
2020-10-26 14:12:06 -0500

    updated session_store and cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
 
     allow do
-        origin "archival-streaming-base.netlify.app"
+        origins "archival-streaming-base.netlify.app"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
 end
