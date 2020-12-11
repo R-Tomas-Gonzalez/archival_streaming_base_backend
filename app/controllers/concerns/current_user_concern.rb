5317dc39637349b6cb4107436c0ed2f07a0a0bc4
R-Tomas-Gonzalez <tomas@rtomasgonzalez.com>
2020-12-11 01:22:18 -0600

    added concern again

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -5,7 +5,7 @@ extend ActiveSupport::Concern
     end
 
     def set_current_user
-        if session[:user]
+        if session[:user_id]
             @current_user = User.find(session[:user_id])
         end
     end
