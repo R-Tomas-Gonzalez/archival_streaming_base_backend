de43745821b44405bad6c9fc7d9edb661b0caf34
R-Tomas-Gonzalez <tomas@rtomasgonzalez.com>
2020-12-27 17:12:28 -0600

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :game_favorites
   resources :movie_favorites
   resources :users
-  resources :sessions, only: [:create]
+  resources :sessions
   resources :registrations, only: [:create]
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
