69908845b021b4daf604cfa3a7ebda3bbb571849
R-Tomas-Gonzalez <tomas@rtomasgonzalez.com>
2021-01-02 14:53:44 -0600

    changed the concern

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -6,7 +6,7 @@ extend ActiveSupport::Concern
 
     def set_current_user
 
-        # current_user = User.find_by_id(session[:user_id])
+        @current_user = User.find_by(id: session[:user_id])
 
         puts "this is the current_user in the concern: #{@current_user}"
 
