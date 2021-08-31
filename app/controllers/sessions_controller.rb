af70307d963b2fa73bc8c6b18ccd7fcc85415166
R-Tomas-Gonzalez <tomas@rtomasgonzalez.com>
2021-08-31 15:58:52 -0500

    session problem on backend

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,11 +18,16 @@ class SessionsController < ApplicationController
     end
 
     def logged_in
+        if @current_user
             render json: {
                 logged_in: true,
                 user: @current_user
             }
-
+        else
+            render json: {
+                logged_in: false
+            }
+        end
     end
 
     def logout
