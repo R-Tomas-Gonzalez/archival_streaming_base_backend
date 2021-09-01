ea454e2075bb8f1e763e8a724ec7b9f46d33558e
Tomas Gonzalez <63771558+R-Tomas-Gonzalez@users.noreply.github.com>
2021-09-01 10:04:52 -0500

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
 
-    Rails.application.config.session_store :cookie_store, key: '_archival_streaming_base', :domain => :all
+    Rails.application.config.session_store :cookie_store, key: '_archival_streaming_base'
