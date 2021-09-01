843a05db5f46f7a62395bf80896dce26967c525a
Tomas Gonzalez <63771558+R-Tomas-Gonzalez@users.noreply.github.com>
2021-09-01 10:11:27 -0500

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
 
-    Rails.application.config.session_store :cookie_store, key: '_archival_streaming_base'
+    Rails.application.config.session_store :cookie_store, key: '_archival_streaming_base', domain: "https://archival-streaming-base.herokuapp.com"
